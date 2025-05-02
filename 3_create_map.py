4a91e77300bf8486f5a0e62945c12fb3d7de6fee
Leonardo Valadão <leonardo.m.valadao@hotmail.com>
2025-05-02 15:51:10 -0300

    Round up final

--- 3_create_map.py
+++ 3_create_map.py
@@ -1,9 +1,13 @@
+### Imports
+
 import pandas as pd
 import plotly.express as px
 import plotly.colors as pc
 import numpy as np
 
 from map_country_pt_en import countries_map_pt_en
+
+### Carregar tabelas gerais
 
 df_citacoes = pd.read_csv('Data/Numero_Citacoes.csv')
 df_programas = pd.read_csv('Data/Numero_Programas.csv')
@@ -12,12 +16,16 @@ df['Country'] = df['Pais'].map(countries_map_pt_en)
 df = df.rename(columns={'NumCit': 'Citações', 'NumProgramas': 'Programas'})
 df["Citações_Log"] = np.log1p(df["Citações"])  
 
+### Tomar qcuts para anotação do mapa de calor
+
 _, list_values = pd.qcut(df['Citações'], 10, duplicates='drop', retbins=True)
 list_values = [round(i) for i in list_values]
 # list_values = np.arange(1, max(df['Citações'])+1, round(max(df['Citações'])/10))
 
 tick_vals = np.log1p(list_values)
 tick_text = [str(v) for v in list_values]
+
+### Definir figura 
 
 greys = pc.sequential.Greys
 custom_greys = greys[1:-1]  
@@ -41,8 +49,6 @@ fig.update_traces(
     customdata=df.values
 )
 
-
-
 fig.update_coloraxes(
     colorbar=dict(
         
@@ -60,5 +66,7 @@ fig.update_layout(
     ),
     margin=dict(l=0, r=0, t=0, b=0)  # Remove extra margin
 )
+
+### Salvar
 
 fig.write_html('my_map.html', include_plotlyjs="cdn", full_html=True)
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# xadrez_verbal_dashboard
+# Análise por transcrição do Podcast Xadrez Verbal
+
+Este pequeno projeto tem como objetivo analisar os países mais mencionados no podcast Xadrez Verbal entre os episódios 385 e 405 (de julho a dezembro de 2024), tanto em termos de número total de citações quanto no número de episódios em que cada país foi citado.
+
+A transcrição dos episódios foi feita utilizando a ferramenta <a href="https://openai.com/index/whisper/" target="_blank"  >Whisper</a> da OpenAI.
+
+Acesse o resultado final <a href="https://leonardovaladao.github.io/xadrez_verbal_dashboard/" target="_blank" >clicando aqui.</a>.
